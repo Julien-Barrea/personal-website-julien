06aef3e095229c6c92bca93477cad0c2abbbc9df
Julien <julien@barrea.lu>
2025-10-12 23:21:51 +0200

    feat(): create website structure with some dummy values

---
+++ src/app/projects/components/project-card/project-card.component.ts
@@ -0,0 +1,13 @@
+
+import { Component } from "@angular/core";
+
+@Component({
+  standalone: true,
+  selector: 'app-project-card',
+  templateUrl: './project-card.component.html',
+  styleUrls: ['./project-card.component.scss'],
+  imports: [  ]
+})
+export class ProjectCardComponent{
+
+}
--- src/app/projects/components/projects/projects.component.ts
+++ src/app/projects/components/projects/projects.component.ts
@@ -1,13 +1,14 @@
 
 import { Component } from "@angular/core";
 import { PageComponent } from "../../../shared/components/page/page.component";
+import { ProjectCardComponent } from "../project-card/project-card.component";
 
 @Component({
   standalone: true,
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
-  imports: [ PageComponent ]
+  imports: [ PageComponent, ProjectCardComponent ]
 })
 export class ProjectsComponent{
 
